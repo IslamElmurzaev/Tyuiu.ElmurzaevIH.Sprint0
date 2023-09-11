e27485ccaa93278e5e8de20ed5e31a31cea374e7
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-09-11 22:21:25 +0500

    Task4

---
+++ Tyuiu.ElmurzaevIH.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c2ce3e7-9cfa-4726-8f88-565e82873938")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ElmurzaevIH.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ElmurzaevIH.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.ElmurzaevIH.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 5));
+            Console.WriteLine(DataService.Subtraction(10, 5));
+            Console.WriteLine(DataService.Multiplication(10, 5));
+            Console.WriteLine(DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
